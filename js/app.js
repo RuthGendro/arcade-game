13ed3ac34947619f375f3739ed7f0a9fa23d0a8a
Ruth Gendro <ruthjg90@gmail.com>
2016-10-11 10:28:59 -0700

    finished game, cleaned up files, removed comments

--- js/app.js
+++ js/app.js
@@ -1,66 +1,62 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    this.x = x;
-    this.y = y;
-    this.speed = speed
+var Enemy = function(x, y, speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.x += this.speed * dt;
+  this.x += this.speed * dt;
 
-    // Ensures the game runs at the same speed for
-    // all computers.
+// dt ensures the game runs at the same speed for
+// all computers.
 
-    // resets enemy bug when it reaches canvas wall
-    if (this.x > 505) {
-        this.reset();
-    }
-
+// resets enemy bug when it reaches canvas wall
+  if (this.x > 505) {
+    this.reset();
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// resets the enemy bug back to start of canvas
 Enemy.prototype.reset = function() {
-    this.x =-200;
+  this.x = -200;
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-// Set player x/y coordinates
+// Sets player x and y starting coordinates
 var startX = 200;
 var startY = 400;
 
 var Player = function() {
-    this.x = startX;
-    this.y = startY;
-    this.sprite = 'images/char-boy.png';
+  this.x = startX;
+  this.y = startY;
+  this.sprite = 'images/char-boy.png';
 };
 
 
 Player.prototype.update = function() {
 
-  // x axis boundaries
+  //set x axis boundaries
   if (this.x < 0) {
     this.x = 0;
   } else if (this.x > 400) {
     this.x = 400;
   }
-  // y axis boundaries
+  //set y axis boundaries
   else if (this.y > 400) {
     this.y = 400;
   }
-  // reset if player reaches water
+  //reset if player reaches water
   else if (this.y < 0) {
     this.reset();
   }
@@ -78,11 +74,12 @@ Player.prototype.render = function() {
 };
 
 
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 
 var allEnemies = [];
-
 
 // push enemies into allEnemies array, 3 enemies total
 // sets Y coordinate for each enemy to 65, 145, and 225 by using incrementer
@@ -91,30 +88,28 @@ var allEnemies = [];
 for (var i = 0; i < 3; i++) {
   var enemyY = 65 + 80 * i;
   var enemyX = Math.floor(Math.random() * 30);
-  var enemySpeed = 60 + Math.floor(Math.random() * 150);
+  var enemySpeed = 50 + Math.floor(Math.random() * 150);
   allEnemies.push(new Enemy(enemyX, enemyY, enemySpeed));
+}
 
 
 // Place the player object in a variable called player
-
-    var player = new Player();
-
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
 
-    // switch statement takes the key event listener and adjusts x or y accordingly
+// switch statement takes the key event listener and adjusts x or y accordingly
 Player.prototype.handleInput = function(key) {
   switch (key) {
     case 'up':
